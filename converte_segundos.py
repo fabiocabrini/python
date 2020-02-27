135ee7fd2b6c84d24451cdf4a0d8cc71fb090ecd
Fábio Henrique Cabrini <fabio.cabrini@gethelix.org>
2020-02-26 23:50:53 -0300

    Create converte_segundos.py

---
+++ converte_segundos.py
@@ -0,0 +1,7 @@
+segundos_str = input("Por favor, entre com o número de segundos que deseja converter: ")
+total_segs = int(segundos_str)
+horas = total_segs // 3600
+segs_restantes = total_segs % 3600
+minutos =  segs_restantes // 60
+segs_restantes_final = segs_restantes  % 60
+print("Hrs=", horas, "mins=", minutos, "segs=", segs_restantes_final)
